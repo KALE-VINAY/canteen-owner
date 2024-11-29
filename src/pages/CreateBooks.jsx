89fdef53ff5549a7f541e6db48a630559734e77f
KALE-VINAY <vinaykale321@gmail.com>
2024-11-30 00:38:21 +0530

    add original api path

--- src/pages/CreateBooks.jsx
+++ src/pages/CreateBooks.jsx
@@ -24,11 +24,11 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('https://shop-status-zenu.onrender.com/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
-        navigate('/');
+        navigate('https://shop-status-zenu.onrender.com');
       })
       .catch((error) => {
         setLoading(false);
--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://localhost:5555/books/${id}`)
+      .delete(`https://shop-status-zenu.onrender.com/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
